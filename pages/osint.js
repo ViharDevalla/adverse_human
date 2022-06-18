5ff0403dbf080771657be0e61cac69b3878801a0
Vihar Devalla <vihardevalla@gmail.com>
2022-06-18 06:55:09 +0530

    idk anymore

--- pages/osint.js
+++ pages/osint.js
@@ -1,6 +1,26 @@
+import {useState} from 'react';
 import Image from "next/image"
+import axios from "axios"
 
-function osint() {
+
+
+function Osint() {
+    const [sherlockData,setSherlockData] = useState(null)
+    const [username,setUsername] = useState('')
+
+
+    async function handleOSINT(username) {
+        const url = '/api/osint'
+        console.log("Username",username)
+        const response = await axios.post(url, 
+            {
+                name: username
+            })
+            if(response.data){
+                setSherlockData(response.data)
+            }
+    }
+
   return (
     <div className="pt-48 px-24 bg-black text-white min-h-screen">
         <h1 className="text-4xl font-bold">
@@ -21,20 +41,25 @@ function osint() {
             Search By Username
         </h2>
 
-        <form className="grid grid-cols-5 p-5 rounded mt-5">
+        <div className="grid grid-cols-5 p-5 rounded mt-5">
             <div className="col-span-5 flex flex-col">
                 <label className="uppercase font-bold text-xs pb-2">Username</label>
-                <input type="text" className="px-2 py-2 rounded"  />
+                <input type="text" className="text-black px-2 py-2 rounded" value={username} onChange={(e)=>{setUsername(e.target.value)}}  />
             </div>
-            <button className=" mt-5 col-span-1 flex items-center justify-center px-4 py-2 bg-blue-500 text-white rounded-xl">
+            <button className=" mt-5 col-span-1 flex items-center justify-center px-4 py-2 bg-blue-500 text-white rounded-xl"
+            onClick={(e)=>{handleOSINT(username)}}
+            >
                 Submit
             </button>
             
-        </form>
+            <div className="text-white">
+                {sherlockData}
+            </div>
+        </div>
         
         
     </div>
   )
 }
 
-export default osint
+export default Osint
